2dd5f331ef2b3fca2209f7a842bb87bc96436b88
José Quiñónez <jquinonez@tsc.com.gt>
2025-01-29 19:33:03 -0600

    Agregar script de ejercicios y comando de inicio en package.json

---
+++ tests/excercises.js
@@ -0,0 +1,47 @@
+// 1. Escribe un comentario en una línea
+
+// "Hola Mundo"
+
+// 2. Escribe un comentario en varias líneas
+
+/*
+    "Hola Mundo"
+    "Adios Mundo"
+*/
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+
+let a = 1;
+let b = "2";
+let c = true;
+let d = false;
+let e = null;
+let f = undefined;
+
+// 4. Imprime por consola el valor de todas las variables
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+
+// 5. Imprime por consola el tipo de todas las variables
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+// 9. A continuación, modifica los valores de las constantes
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
