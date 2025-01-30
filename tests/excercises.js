2f7ac6b2a79e36db98c96b17f703f0f35f89ae70
José Quiñónez <jquinonez@tsc.com.gt>
2025-01-30 03:22:54 -0600

    Agregar ejercicios de manipulación de datos primitivos y operaciones en el archivo excercises.js

--- tests/excercises.js
+++ tests/excercises.js
@@ -9,6 +9,7 @@
     "Adios Mundo"
 */
 
+/*
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
 
 let a = 1;
@@ -37,11 +38,153 @@ console.log(typeof e);
 console.log(typeof f);
 
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+a = 2;
+b = "3";
+c = false;
+d = true;
+e = undefined;
+f = null;
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+a = "1";
+b = 1;
+c = "true";
+d = false;
+e = null;
+f = undefined;
 
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+const g = 1;
+const h = "2";
+const i = true;
+const j = false;
+const k = null;
+const l = undefined;
 
 // 9. A continuación, modifica los valores de las constantes
+// g = 2;
+// h = "3";
+// i = false;
+// j = true;
+// k = undefined;
+// l = null;
 
 // 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
+
+// 1. Crea una variable para cada operación aritmética
+
+let suma = 1 + 2;
+let resta = 10 - 5;
+let multiplicacion = 3 * 4;
+let division = 10 / 2;
+let modulo = 10 % 3;
+
+// 2. Crea una variable para cada tipo de operación de asignación,
+//    que haga uso de las variables utilizadas para las operaciones aritméticas
+
+suma 
+
+// 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
+
+console.log(1 < 2);
+console.log(2 > 1);
+console.log(1 <= 2);
+console.log(2 >= 1);
+console.log(1 != 2);
+
+// 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
+
+console.log(1 > 2);
+console.log(2 < 1);
+console.log(1 >= 2);
+console.log(2 <= 1);
+console.log(1 == 2);
+
+// 5. Utiliza el operador lógico and
+
+console.log(1 < 2 && 2 > 1);
+
+// 6. Utiliza el operador lógico or
+
+console.log(1 < 2 || 1 > 2);
+
+// 7. Combina ambos operadores lógicos
+
+console.log(1 < 2 && 1 > 2 || 1 != 2);
+
+// 8. Añade alguna negación
+
+console.log(!(1 < 2 && 1 > 2 || 1 != 2));
+
+// 9. Utiliza el operador ternario
+
+let ternario = 1 < 2 ? "Verdadero" : "Falso";
+
+// 10. Combina operadores aritméticos, de comparáción y lógicas
+
+console.log(1 < 2 && 1 > 2 || 1 != 2 ? "Verdadero" : "Falso");
+
+// 1. Concatena dos cadenas de texto
+
+let concatenacion = "Hola " + "Mundo";
+console.log(concatenacion);
+
+// 2. Muestra la longitud de una cadena de texto
+
+let longitud = concatenacion.length;
+console.log(longitud);
+
+// 3. Muestra el primer y último carácter de un string
+
+let primer = concatenacion[0];
+let ultimo = concatenacion[longitud - 1];
+console.log(primer);
+console.log(ultimo);
+
+// 4. Convierte a mayúsculas y minúsculas un string
+
+let mayusculas = concatenacion.toUpperCase();
+let minusculas = concatenacion.toLowerCase();
+console.log(mayusculas);
+console.log(minusculas);
+
+// 5. Crea una cadena de texto en varias líneas
+
+let varias = `Hola
+Mundo`;
+console.log(varias);
+
+// 6. Interpola el valor de una variable en un string
+
+let nombre = "José"
+let interpolado = `Hola ${nombre}`;
+console.log(interpolado);
+
+// 7. Reemplaza todos los espacios en blanco de un string por guiones
+
+let reemplazados = interpolado.replace(" ", "-");
+console.log(reemplazados);
+
+// 8. Comprueba si una cadena de texto contiene una palabra concreta
+
+let palabra = "Mundo";
+let contiene = interpolado.includes(palabra);
+let contiene2 = varias.includes("Mundo");
+console.log(contiene);
+console.log(contiene2);
+
+// 9. Comprueba si dos strings son iguales
+
+let iguales2 = "Hola José";
+let iguales = interpolado == iguales2;
+console.log(iguales);
+
+// 10. Comprueba si dos strings tienen la misma longitud
+
+let longitud1 = interpolado.length;
+let longitud2 = iguales2.length;
+let comparación = longitud1 == longitud2 ? "Igual" : "Diferente";
+console.log(comparación);
+
+*/
+
